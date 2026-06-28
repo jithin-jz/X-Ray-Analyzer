5eb464395e181efe7107cdf58b92aa4beaec60c8
Anshif <anshifanu4014@gmail.com>
2026-06-28 12:52:03 +0530

    Add custom EKS admin IAM policy for GitHub Actions

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -4,6 +4,26 @@ module "github_oidc" {
   version = "~> 5.0"
 }
 
+resource "aws_iam_policy" "eks_admin" {
+  name        = "github-actions-eks-admin-policy"
+  description = "Allows GitHub Actions to describe and access EKS cluster"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListClusters",
+          "eks:AccessKubernetesApi"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 module "iam_github_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-role"
   version = "~> 5.0"
@@ -15,7 +35,7 @@ module "iam_github_role" {
 
   policies = {
     ECRPushPull = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
-    EKSAdmin    = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    EKSAdmin    = aws_iam_policy.eks_admin.arn
   }
 }
 
